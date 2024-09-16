18e1c3b0523d6b3f612fbbce68e8f75985cad47c
Wally1403 <christobaudouin@gmail.com>
2024-09-16 12:28:10 +0200

    name the first class

--- src/components/DessertItem.js
+++ src/components/DessertItem.js
@@ -1,7 +1,65 @@
-function DessertItem({cover, category, name, price}) {
+function DessertItem({cart, updateCart, cover, category, name, price}){
+
+    function addAmountToCart(name, price) {
+
+        const currentDessertSaved = cart.find((dessert) => dessert.name === name)
+    
+        const cartFilteredCurrentDessert = cart.filter(
+          (dessert) => dessert.name !== name
+        )
+        updateCart([
+          ...cartFilteredCurrentDessert,
+          {name, price, amount: currentDessertSaved.amount + 1}]
+        )
+      } 
+      function addToCart(name, price) {
+          updateCart([...cart, {name, price, amount: 1}])
+      }
+      function removeToCart(name, price) {
+        const currentPlantSaved = cart.find((plant) => plant.name === name)
+            if (currentPlantSaved) {
+                const cartFilteredCurrentPlant = cart.filter(
+                    (plant) => plant.name !== name
+                )
+                if (currentPlantSaved.amount === 1) {
+                    updateCart(
+                        cartFilteredCurrentPlant
+                    )
+                } else {
+                    updateCart([
+                        ...cartFilteredCurrentPlant,
+                        { name, price, amount: currentPlantSaved.amount - 1 }
+                    ])
+                }
+            }
+    }
+    
+    function buttonToAdd(name, price) {
+        const currentDessertSaved = cart.find((dessert) => dessert.name === name)
+
+        if(currentDessertSaved) {
+            return (<div>
+            <button onClick={() => removeToCart(name, price)}>-</button>
+            {currentDessertSaved.amount}
+            <button onClick={() => addAmountToCart(name, price)}>+</button>
+            </div>)
+            
+        } else {
+            return (
+                <button className="dc-productList-item-button-addToCart" onClick={() => addToCart(name, price)}>
+                    <img alt="logo-caddie" src="d"/>
+                    <p>Add to Cart</p>
+                </button>
+            )
+        }
+    }
+
     return (
-        <div>
-            <img src={cover} alt="image du gâteau"/>
+        <div className="dc-productList-item">
+            <div className="dc-productList-item-image-and-button">
+                <img src={cover} alt="image du gâteau"/>
+                {buttonToAdd(name, price)}
+            </div>
             <p>{category}</p>
             <h3>{name}</h3>
             <h4>${price}</h4>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,74 +1,30 @@
 import { dessertList } from "../data/data"
 import DessertItem from "./DessertItem"
-import { useState } from "react"
 
 function ShoppingList( {cart, updateCart} ) {
 
-  function addAmountToCart(name, price) {
-
-    const currentDessertSaved = cart.find((dessert) => dessert.name === name)
-
-    const cartFilteredCurrentDessert = cart.filter(
-      (dessert) => dessert.name !== name
-    )
-    updateCart([
-      ...cartFilteredCurrentDessert,
-      {name, price, amount: currentDessertSaved.amount + 1}]
-    )
-  } 
-  function addToCart(name, price) {
-      updateCart([...cart, {name, price, amount: 1}])
-  }
-  function removeToCart(name, price) {
-    const currentPlantSaved = cart.find((plant) => plant.name === name)
-		if (currentPlantSaved) {
-			const cartFilteredCurrentPlant = cart.filter(
-				(plant) => plant.name !== name
-			)
-			if (currentPlantSaved.amount === 1) {
-				updateCart(
-					cartFilteredCurrentPlant
-				)
-			} else {
-				updateCart([
-					...cartFilteredCurrentPlant,
-					{ name, price, amount: currentPlantSaved.amount - 1 }
-				])
-			}
-		}
-}
-
-  function buttonToAdd(name, price) {
-    const currentDessertSaved = cart.find((dessert) => dessert.name === name)
-
-    if(currentDessertSaved) {
-      return (<div>
-        <button onClick={() => removeToCart(name, price)}>-</button>
-        {currentDessertSaved.amount}
-        <button onClick={() => addAmountToCart(name, price)}>+</button>
-      </div>)
-      
-    } else {
-      return <button onClick={() => addToCart(name, price)}>Add to Cart</button>
-    }
-  }
+  
 
   return(
     <div>
       <h1>Desserts</h1>
+      <div className="dc-productList">
         {dessertList.map(({ cover, id, name, category, price }) =>
           (
-            <div key={id}>
+            <div key={id} className="dc-productList-row">
                     <DessertItem 
+                      cart={cart}
+                      updateCart = {updateCart}
                       cover= {cover}
                       category= {category}
                       name= {name}
-                      price= {price.toFixed(2)}
+                      price= {price}
                     />
-                    {buttonToAdd(name, price)}
+              
             </div>
           )
         )}
+      </div>
     </div>
   )
 }
